fd09f9406ee8fd1629089388094da94f066eba61
Eric Cabrel <tericcabrel@yahoo.com>
2020-01-01 22:19:41 +0100

    Initial commit

---
+++ src/main/java/com/tericcabrel/authorization/IdentityAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tericcabrel/authorization/IdentityAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tericcabrel.authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
