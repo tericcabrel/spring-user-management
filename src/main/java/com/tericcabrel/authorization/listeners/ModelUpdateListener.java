4c46d048230115b1ef80f5b84c76c805d2abe871
Eric Cabrel <tericcabrel@yahoo.com>
2020-01-02 21:44:21 +0100

    Create a listener set createdAt and UpdateAt automatically when creating a document

--- src/main/java/com/tericcabrel/authorization/listeners/ModelUpdateListener.java
+++ src/main/java/com/tericcabrel/authorization/listeners/ModelUpdateListener.java
@@ -2,21 +2,27 @@ package com.tericcabrel.authorization.listeners;
 
 import com.tericcabrel.authorization.models.BaseModel;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
 import org.springframework.data.mongodb.core.mapping.event.BeforeSaveEvent;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
 @Component
-public class BeforeSaveListener extends AbstractMongoEventListener<BaseModel> {
+public class ModelUpdateListener extends AbstractMongoEventListener<BaseModel> {
     @Override
     public void onBeforeSave(BeforeSaveEvent<BaseModel> event) {
         super.onBeforeSave(event);
+
+    }
+
+    @Override
+    public void onBeforeConvert(BeforeConvertEvent<BaseModel> event) {
+        super.onBeforeConvert(event);
+
         Date dateNow = new Date();
 
         event.getSource().setCreatedAt(dateNow);
         event.getSource().setUpdatedAt(dateNow);
-
-        System.out.println("OnBeforeSave");
     }
 }
