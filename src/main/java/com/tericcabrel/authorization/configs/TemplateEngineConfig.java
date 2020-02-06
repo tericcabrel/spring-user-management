bbed703984964a0495dd782555120542b54c1d7d
Eric Cabrel <tericcabrel@yahoo.com>
2020-02-06 05:55:30 +0100

    Configure web resolver and add index page

--- src/main/java/com/tericcabrel/authorization/configs/TemplateEngineConfig.java
+++ src/main/java/com/tericcabrel/authorization/configs/TemplateEngineConfig.java
@@ -2,8 +2,14 @@ package com.tericcabrel.authorization.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Description;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
@@ -11,19 +17,20 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 import java.util.Collections;
 
 @Configuration
-public class TemplateEngineConfig {
+public class TemplateEngineConfig implements WebMvcConfigurer {
     private static final String EMAIL_TEMPLATE_ENCODING = "UTF-8";
 
     @Bean
+    @Primary
     public TemplateEngine emailTemplateEngine() {
         final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         // Resolver for HTML emails (except the editable one)
-        templateEngine.addTemplateResolver(htmlTemplateResolver());
+        templateEngine.addTemplateResolver(emailTemplateResolver());
 
         return templateEngine;
     }
 
-    private ITemplateResolver htmlTemplateResolver() {
+    private ITemplateResolver emailTemplateResolver() {
         final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
         templateResolver.setOrder(1);
         templateResolver.setResolvablePatterns(Collections.singleton("html/*"));
@@ -34,5 +41,50 @@ public class TemplateEngineConfig {
         templateResolver.setCacheable(false);
 
         return templateResolver;
+    }
+
+    @Bean
+    @Description("Thymeleaf template resolver serving HTML 5")
+    public ClassLoaderTemplateResolver webTemplateResolver() {
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+
+        templateResolver.setPrefix("templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        // Template cache is true by default.
+        // Set to false if you want templates to be automatically updated when modified.
+        templateResolver.setCacheable(false);
+
+        return templateResolver;
+    }
+
+    @Bean
+    @Description("Thymeleaf template engine with Spring integration")
+    public SpringTemplateEngine webTemplateEngine() {
+
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(webTemplateResolver());
+
+        return templateEngine;
+    }
+
+    @Bean
+    @Description("Thymeleaf view resolver")
+    public ViewResolver viewResolver() {
+
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+
+        viewResolver.setTemplateEngine(webTemplateEngine());
+        viewResolver.setCharacterEncoding("UTF-8");
+
+        return viewResolver;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
     }
 }
