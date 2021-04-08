e46c9415cbd3f61f10da55ac21bb8962aed89669
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-04-08 23:52:45 +0200

    feat: create endpoint to create user's admin

---
+++ src/main/java/com/tericcabrel/authorization/controllers/AdminController.java
@@ -0,0 +1,54 @@
+package com.tericcabrel.authorization.controllers;
+
+import static com.tericcabrel.authorization.utils.Constants.ROLE_ADMIN;
+import static com.tericcabrel.authorization.utils.Constants.ROLE_NOT_FOUND_MESSAGE;
+import com.tericcabrel.authorization.exceptions.ResourceNotFoundException;
+import com.tericcabrel.authorization.models.dtos.CreateUserDto;
+import com.tericcabrel.authorization.models.entities.Role;
+import com.tericcabrel.authorization.models.entities.User;
+import com.tericcabrel.authorization.models.response.UserResponse;
+import com.tericcabrel.authorization.services.interfaces.RoleService;
+import com.tericcabrel.authorization.services.interfaces.UserService;
+import java.util.Optional;
+import javax.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/admins")
+public class AdminController {
+  private final RoleService roleService;
+
+  private final UserService userService;
+
+
+  public AdminController(RoleService roleService, UserService userService) {
+    this.roleService = roleService;
+    this.userService = userService;
+  }
+
+  @PreAuthorize("hasRole('ROLE_SUPER_ADMIN')")
+  @PostMapping(value = "")
+  public ResponseEntity<UserResponse> create(@Valid @RequestBody CreateUserDto createUserDto)
+      throws ResourceNotFoundException {
+    Optional<Role> roleUser = roleService.findByName(ROLE_ADMIN);
+
+    if (roleUser.isEmpty()) {
+      throw new ResourceNotFoundException(ROLE_NOT_FOUND_MESSAGE);
+    }
+
+    createUserDto.setRole(roleUser.get())
+        .setConfirmed(true)
+        .setEnabled(true);
+
+    User user = userService.save(createUserDto);
+
+    return ResponseEntity.ok(new UserResponse(user));
+  }
+}
