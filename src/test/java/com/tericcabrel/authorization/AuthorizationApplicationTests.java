f3b3f15106785b2610f962c8d3037d410d38086c
Eric Cabrel TIOGO <tericcabrel@yahoo.com>
2021-05-14 23:25:39 +0200

    test: initialize test containers with mongodb

--- src/test/java/com/tericcabrel/authorization/AuthorizationApplicationTests.java
+++ src/test/java/com/tericcabrel/authorization/AuthorizationApplicationTests.java
@@ -1,13 +1,45 @@
 package com.tericcabrel.authorization;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+import com.tericcabrel.authorization.models.entities.Role;
+import com.tericcabrel.authorization.repositories.RoleRepository;
+import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@ActiveProfiles("test")
+@Testcontainers
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class AuthorizationApplicationTests {
+	static final MongoDBContainer MONGO_DB_CONTAINER = new MongoDBContainer(DockerImageName.parse("mongo:4.4.6"));
+
+	@BeforeAll
+	static void beforeAll() {
+		MONGO_DB_CONTAINER.start();
+	}
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.data.mongodb.uri", MONGO_DB_CONTAINER::getReplicaSetUrl);
+	}
+
+	@Autowired
+	RoleRepository roleRepository;
 
 	@Test
 	void contextLoads() {
-	}
+		List<Role> roleList = roleRepository.findAll();
 
+		assertThat(roleList.size()).isEqualTo(3);
+	}
 }
---
+++ src/test/java/com/tericcabrel/authorization/controllers/AuthControllerIT.java
@@ -0,0 +1,5 @@
+package com.tericcabrel.authorization.controllers;
+
+public class AuthControllerIT {
+
+}
